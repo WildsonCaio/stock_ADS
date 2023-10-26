d9701159757368309fc3069cacf863a0bd308669
WILDSON CAIO FELIPE <wildson.felipe@prof.sc.senac.br>
2023-10-25 21:36:15 -0300

    Include method POST

--- stockADS/urls.py
+++ stockADS/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('add-product/', views.add_product, name='add_product'),
+    path('delete-product/<int:id>/', views.delete_product, name='delete-product' ),
     path('product-detail/<int:id>/', views.product_detail, name='product-detail' )
 ]
 
