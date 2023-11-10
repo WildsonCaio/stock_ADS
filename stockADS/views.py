8ad2d5cda7472516b3b3e21f2bd8f6ee85161b53
WILDSON CAIO FELIPE <wildson.felipe@prof.sc.senac.br>
2023-11-09 21:13:51 -0300

    login and register

--- stockADS/views.py
+++ stockADS/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Products, Categories
 from random import randint
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(redirect_field_name='login')
 def index(request):
     produtos = Products.objects.all()
     return render(request, 'pages/index.html', {'produtos': produtos})
